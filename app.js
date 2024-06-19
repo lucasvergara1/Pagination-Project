a371a383bdc129605c668c7321794648aef83f15
Lucas <lucas.vergara@live.com>
2024-06-18 22:47:18 -0300

    :construction: Create page buttons

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
 
 const title = document.querySelector('.section-title h1')
+const btnContainer = document.querySelector('.btn-container')
 
+let index = 0
+let pages = []
+
+const setupUI = () => {
+    displayFollowers(pages[index])
+    displayButtons(btnContainer, pages, index)
+}
 
 const init = async() => {
-    const followers = await fetchFollowers(); void
-    displayFollowers(paginate(followers)[0])
+    const followers = await fetchFollowers();
     title.textContent = 'pagination'
-    const pages = paginate(followers)
-    console.log(pages)
+    pages = paginate(followers)
+    setupUI()
 
 }
 
