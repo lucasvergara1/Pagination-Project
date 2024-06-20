488bf9230a6f15a5cfec35a1953e701126217165
Lucas <lucas.vergara@live.com>
2024-06-19 22:15:13 -0300

    :sparkles: End of the project

--- app.js
+++ app.js
@@ -22,4 +22,26 @@ const init = async() => {
 
 }
 
+
+btnContainer.addEventListener('click', function(e){
+    if(e.target.classList.contains('btn-container')) return
+    if(e.target.classList.contains('page-btn')){
+       index = parseInt(e.target.dataset.index)
+
+    }
+    if(e.target.classList.contains('next-btn')){
+        index++
+        if(index > pages.length -1){
+            index = 0
+        }
+    }
+    if (e.target.classList.contains("prev-btn")) {
+      index--;
+      if (index < pages.length - 1) {
+        index = 0;
+      }
+    }
+    setupUI()
+})
+
 window.addEventListener('load',init);
